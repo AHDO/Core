c85ebfcee150f226a2e49df619deb08b8b97d064
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-04-18 18:09:29 -0500

    Added generators to Core component

---
+++ Generator/Interfaces/GeneratorInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Component\Core\Generator\Interfaces;
+
+interface GeneratorInterface
+{
+    /**
+     * Generates a hash.
+     *
+     * @param int $length Length of string generated
+     *
+     * @return string Generation
+     */
+    public function generate($length = null);
+}
